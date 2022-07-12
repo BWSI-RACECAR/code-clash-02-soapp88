280fb3fb1750e88e580615427b392eb2e5a5b46d
soapp88 <91401787+soapp88@users.noreply.github.com>
2022-07-12 07:15:50 -0700

    Update speedometer.py

--- speedometer.py
+++ speedometer.py
@@ -34,7 +34,7 @@ class Solution:
         # return type: list or bool
 
         # TODO: Write code below to return a list with the solution to the prompt
-        for i in range(len(ary)):
+        for i in range(len(ary) - 1):
             for j in range(i + 1, len(ary)):
                 if ary[i] + ary[j] == target:
                     return [i, j]
